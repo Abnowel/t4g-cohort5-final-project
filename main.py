ad22c7eb696a535c0020350c703d6b55563bf5b9
Abnowel <abimaame77@gmail.com>
2026-08-03 21:41:43 +0000

    Set up FastAPI project

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+# Create our FastAPI application
+app = FastAPI()
+
+
+# Our first API endpoint
+@app.get("/")
+def read_root():
+    return {
+        "message": "Welcome to the Jewelry Order and Inventory Management System"
+    }
