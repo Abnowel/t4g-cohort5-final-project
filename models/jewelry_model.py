ffcffa6a3f984d1dcd9b1d6c7f23785536cb167b
Abnowel <abimaame77@gmail.com>
2026-08-05 17:40:34 +0000

    Created Jewelry Model

---
+++ models/jewelry_model.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, String, Numeric, Integer
+from models.base_model import Base
+from utils.uuid_generate import generate_uuid
+
+
+# Creating Jewelry model
+class Jewelry(Base):
+    """Represents jewelry products in the system."""
+
+    __tablename__ = "jewelry"
+
+    id = Column(String(60), primary_key=True, default=generate_uuid)
+    name = Column(String(100), nullable=False)
+    category = Column(String(50), nullable=False)
+    material = Column(String(50), nullable=False)
+    price = Column(Numeric(10, 2), nullable=False)
+    stock_quantity = Column(Integer, nullable=False)
+
+
+    def __str__(self):
+        return (
+            f"Jewelry(id={self.id}, name={self.name}, price=GHC{self.price})"
+        )
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "category": self.category,
+            "material": self.material,
+            "price": self.price,
+            "stock_quantity": self.stock_quantity
+        }
