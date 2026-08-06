8d45e06eaa9f83c0de434fc18081700f354fa497
Abnowel <abimaame77@gmail.com>
2026-08-06 15:45:39 +0000

    Implemented create customer service

--- services/customer_service.py
+++ services/customer_service.py
@@ -7,4 +7,14 @@ class CustomerService:
         self.session = session
 
     def create_customer(self, customer_data):
-        
+        customer = Customer(
+    first_name=customer_data.get("first_name"),
+    last_name=customer_data.get("last_name"),
+    phone_number=customer_data.get("phone_number"),
+    email=customer_data.get("email"),
+    address=customer_data.get("address")
+)
+        self.session.add(customer)
+        self.session.commit()
+
+        return customer
