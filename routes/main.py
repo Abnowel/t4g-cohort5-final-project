e6a6177dd2a9fe528ca0531c277e5e47d0b31a95
Abnowel <abimaame77@gmail.com>
2026-08-16 07:33:39 +0000

    served frontend from FastAPI

--- routes/main.py
+++ routes/main.py
@@ -3,15 +3,18 @@ from routes.customer_route import router as customer_router
 from routes.jewelry_route import router as jewelry_router
 from routes.order_route import router as order_router
 from routes.order_item_route import router as order_item_router
+from fastapi.staticfiles import StaticFiles
 
 # Create our FastAPI application
 app = FastAPI()
 
 # Register customer routes with the main application
+app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
 app.include_router(customer_router)
 app.include_router(jewelry_router)
 app.include_router(order_router)
 app.include_router(order_item_router)
+
 
 
 # Our first API endpoint
